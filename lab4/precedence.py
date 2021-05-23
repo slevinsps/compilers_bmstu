99177add5037f02a348c2ca88beb6384d7bc48d6
Ivan Spasenov <spasenovivan@mail.ru>
2021-05-23 13:21:26 +0300

    fix matrix create

--- lab4/precedence.py
+++ lab4/precedence.py
@@ -4,6 +4,7 @@ class Precedence:
         self.R = {}
         self.Lt = {}
         self.Rt = {}
+        self.b_e_symbol = '$'
 
 
     def _getRL(self, grammatic):
@@ -69,17 +70,22 @@ class Precedence:
         for t in self.terminal:
             print(t, end=' ')
         print()
-        for t in self.terminal:
-            print(t, end=' ')
-            for tt in self.terminal:
-                print(self.matrix[t][tt], end= ' ')
+        for t1 in self.terminal:
+            print(t1, end=' ')
+            for t2 in self.terminal:
+                print(self.matrix[t1][t2], end= ' ')
             print()
 
     def buildOperatorPrecedenceMatrix(self, grammatic):
         self._getRL(grammatic)
         self._getRtLt(grammatic)
+        print('Lt ', self.Lt)
+        print('Rt ', self.Rt)
+
         self.matrix = {}
         self.terminal = grammatic['terminal']
+        self.terminal.append(self.b_e_symbol)
+
         for t1 in grammatic['terminal']:
             self.matrix[t1] = {}
             for t2 in grammatic['terminal']:
@@ -90,10 +96,22 @@ class Precedence:
                 for i in range(len(val)):
                     symbol = val[i]
                     if symbol in grammatic['terminal']:
+                        if i < len(val) - 1 and val[i + 1] in grammatic['nonterminal']:
+                            for l in self.Lt[val[i + 1]]:
+                                self.matrix[symbol][l] = '<'
+                        if i < len(val) - 1 and val[i + 1] in grammatic['terminal']:
+                            self.matrix[symbol][val[i + 1]] = '='
+                        elif i < len(val) - 2 and val[i + 2] in grammatic['terminal']:
+                            self.matrix[symbol][val[i + 2]] = '='
+
                         if i != 0 and val[i - 1] in grammatic['nonterminal']:
                             for r in self.Rt[val[i - 1]]:
-                                self.matrix[r][symbol] = '<'
-                        if i != len(val) - 1 and val[i + 1] in grammatic['nonterminal']:
-                            for l in self.Lt[val[i + 1]]:
-                                self.matrix[l][symbol] = '>'
-        
+                                self.matrix[r][symbol] = '>'
+
+            for symbol in self.Lt[grammatic['startsymbol']]:
+                self.matrix[self.b_e_symbol][symbol] = '<'
+
+            for symbol in self.Rt[grammatic['startsymbol']]:
+                self.matrix[symbol][self.b_e_symbol] = '>'
+                        
+                        
